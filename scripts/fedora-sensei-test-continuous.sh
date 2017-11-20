f941b6d1dee94c85e8d1a8376816bbea23f483de
Burlen Loring <bloring@lbl.gov>
2017-11-20 11:08:27 -0800

    actualy do continuous build on fedora-sensei

--- scripts/fedora-sensei-test-continuous.sh
+++ scripts/fedora-sensei-test-continuous.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-/home/sensei/sc17/software/sensei/builds/SENSEI_ctest/fedora-sensei-test.sh Nightly vtk
-/home/sensei/sc17/software/sensei/builds/SENSEI_ctest/fedora-sensei-test.sh Nightly catalyst
-/home/sensei/sc17/software/sensei/builds/SENSEI_ctest/fedora-sensei-test.sh Nightly libsim
+#!/bin/bash -l
+/home/sensei/sc17/software/sensei/builds/SENSEI_ctest/fedora-sensei-test.sh Continuous vtk
+/home/sensei/sc17/software/sensei/builds/SENSEI_ctest/fedora-sensei-test.sh Continuous catalyst
+/home/sensei/sc17/software/sensei/builds/SENSEI_ctest/fedora-sensei-test.sh Continuous libsim
